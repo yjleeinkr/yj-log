e26a6a62bc0c7e1d1af92310f3b4892720e76cd3
yjleeinkr <lucy.lee930216@gmail.com>
2023-05-21 20:17:12 +0900

    add font colors on tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,17 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      primary: '#000000',
+      highlight: '#F79327',
+      secondary: '#999',
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
   plugins: [],
-}
+};
