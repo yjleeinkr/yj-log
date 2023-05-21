86d872f834eb5f03145ce8a7714c94cb9993550d
yjleeinkr <lucy.lee930216@gmail.com>
2023-05-21 20:19:17 +0900

    initial layout for header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import './globals.css';
-import { Inter } from 'next/font/google';
+import { Inter, Noto_Sans_KR } from 'next/font/google';
 import Link from 'next/link';
 
-const inter = Inter({ subsets: ['latin'] });
+const notoKR = Noto_Sans_KR({ weight: ['100', '300', '400', '500', '700', '900'], subsets: ['latin'] });
 
 export const metadata = {
   title: "yj's dev-log",
@@ -12,7 +12,27 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={notoKR.className}>
+        <header className="flex p-4 justify-between">
+          <h1 className="text-lg font-bold whitespace-nowrap">
+            <Link href="/">
+              yj Log <span className="text-highlight">+</span>
+            </Link>
+          </h1>
+          <nav>
+            <Link href="/about" className="p-2 text-base hover:text-highlight transition-all ease-in duration-100">
+              about
+            </Link>
+            <Link href="/posts" className="p-2 text-base hover:text-highlight transition-all ease-in duration-100">
+              posts
+            </Link>
+            <Link href="/contact" className="p-2 text-base hover:text-highlight transition-all ease-in duration-100">
+              contact
+            </Link>
+          </nav>
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
