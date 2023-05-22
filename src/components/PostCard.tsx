2bb59b4b6c583da48029a1d13e23411e4d9dc061
yjleeinkr <lucy.lee930216@gmail.com>
2023-05-22 20:17:45 +0900

    make post cards to be client components

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,32 +1,29 @@
+'use client';
+
 import Image from 'next/image';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
+import { PostProps } from '@/service/posts';
 
-type Category = 'my story' | 'javascript' | 'backend';
-
-export type PostProps = {
-  title: string;
-  description: string;
-  date: string;
-  category: Category;
-  path: string;
-  featured: boolean;
-};
-
-export default function PostCard(Props: PostProps) {
-  const { title, description, date, category, path, featured } = Props;
+export default function PostCard({ post }: { post: PostProps }) {
+  const { title, description, date, category, path } = post;
   const router = useRouter();
 
   return (
-    <article className="drop-shadow-xl rounded-sm" onClick={() => router.push(`/posts/${path}`)}>
-      <Image src={`/img/${path}`} alt={path} width="300" height="300" />
-      <section className="p-2">
-        <p className="text-right">{date}</p>
+    <div
+      className="rounded-md w-full sm:w-[48%] lg:w-[32%] shadow-card overflow-clip cursor-pointer m-1 inline-block hover:shadow-card-pop hover:transition-shadow hover:duration-500"
+      onClick={() => router.push(`/posts/${path}`)}
+    >
+      <div className="">
+        <Image src={`/img/posts/${path}.png`} alt={path} width={100} height={100} className="object-contain w-[100%]" />
+      </div>
+      <section className="py-2 px-3 text-center">
+        <p className="text-right text-xs text-secondary font-light py-1">{date}</p>
         <div className="flex flex-col items-center">
-          <h2 className="text-sm font-bold">{title}</h2>
-          <p className="text-xs font-light">{description}</p>
-          <span className="text-xs rounded-lg bg-point">{category}</span>
+          <h2 className="font-semibold">{title}</h2>
+          <p className="text-sm font-light text-ellipsis overflow-hidden whitespace-nowrap w-full">{description}</p>
+          <span className="text-xs rounded-lg bg-point px-2 py-0.5 my-3">{category}</span>
         </div>
       </section>
-    </article>
+    </div>
   );
 }
