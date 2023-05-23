3e617a477067bcf2cb88c2b88b3851456c757a6c
yjleeinkr <lucy.lee930216@gmail.com>
2023-05-24 02:08:47 +0900

    refac: separate header to be a client component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css';
 import { Noto_Sans_KR } from 'next/font/google';
-import Link from 'next/link';
+import Header from '@/components/Header';
 
 const notoKR = Noto_Sans_KR({ weight: ['100', '300', '400', '500', '700', '900'], subsets: ['latin'] });
 
@@ -9,34 +9,12 @@ export const metadata = {
   description: '주니어 프론트엔드 개발자의 커가는 과정을 남기는 블로그입니다.',
 };
 
-const menus = [
-  { name: 'about', path: '/about' },
-  { name: 'posts', path: '/posts' },
-  { name: 'contact', path: '/contact' },
-];
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={notoKR.className}>
         <div className="h-auto min-h-[100vh]">
-          <header className="flex p-4 justify-between max-w-[90%] lg:max-w-[85%] xl:max-w-[75%] m-zero-auto">
-            <h1 className="text-lg font-bold whitespace-nowrap">
-              <Link href="/">
-                yj Log <span className="text-highlight">+</span>
-              </Link>
-            </h1>
-            <nav>
-              {menus.map((menu, i) => (
-                <Link
-                  href={menu.path}
-                  className="p-2 text-base hover:text-highlight transition-all ease-in duration-100"
-                >
-                  {menu.name}
-                </Link>
-              ))}
-            </nav>
-          </header>
+          <Header />
           {children}
         </div>
         <footer className="h-10 py-2  translate-y-[-50%]">
---
+++ src/components/Header.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+type menuName = 'about' | 'posts' | 'contact';
+type menuTypes = { name: menuName; path: `/${menuName}` };
+
+const menus: menuTypes[] = [
+  { name: 'about', path: '/about' },
+  { name: 'posts', path: '/posts' },
+  { name: 'contact', path: '/contact' },
+];
+
+export default function Header() {
+  const pathname = usePathname();
+
+  return (
+    <header className="flex p-4 justify-between max-w-[90%] lg:max-w-[85%] xl:max-w-[75%] m-zero-auto">
+      <h1 className="text-lg font-bold whitespace-nowrap">
+        <Link href="/">
+          yj Log <span className="text-highlight">+</span>
+        </Link>
+      </h1>
+      <nav>
+        {menus.map((menu, i) => (
+          <Link
+            href={menu.path}
+            key={`menu_${i}`}
+            className={`${
+              pathname === menu.path ? 'text-highlight' : 'text-primary'
+            } p-2 text-base transition-all hover:text-highlight hover:duration-100`}
+          >
+            {menu.name}
+          </Link>
+        ))}
+      </nav>
+    </header>
+  );
+}
