3dc9b814c78f72f7860a9adbbd05379214c1cf4e
yjleeinkr <69417234+yjleeinkr@users.noreply.github.com>
2023-05-24 23:41:17 +0900

    Release 05-24 (#1)
    * add category type
    
    * create posts main page

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,3 +1,12 @@
-export default function PostsPage() {
-  return <div>PostsPage</div>;
+import { getPosts } from '@/service/posts';
+import PostList from '@/components/PostList';
+
+export default async function PostsPage() {
+  const posts = await getPosts();
+
+  return (
+    <div className="max-w-[90%] lg:max-w-[85%] xl:max-w-[80%] m-zero-auto">
+      <PostList posts={posts} />
+    </div>
+  );
 }
