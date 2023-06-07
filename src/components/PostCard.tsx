9dbe952fced14151aa314b85d00be93f82806d62
yjleeinkr <lucy.lee930216@gmail.com>
2023-06-07 22:07:24 +0900

    refac: 텍스트 넘칠 때 자주 쓰는 3줄의 css 라인을 .truncate로 간결하게 css적용

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -18,12 +18,8 @@ export default function PostCard({ post, version }: { post: PostProps; version: 
       <section className="py-2 px-3 text-center">
         <p className="text-right text-[0.6em] sm:text-xs text-secondary font-light">{date}</p>
         <div className="flex flex-col items-center">
-          <h2 className="text-[0.8em] sm:text-sm font-medium py-1 text-ellipsis overflow-hidden whitespace-nowrap">
-            {title}
-          </h2>
-          <p className="text-[0.6em] sm:text-xs font-light text-ellipsis overflow-hidden whitespace-nowrap w-full">
-            {description}
-          </p>
+          <h2 className="text-[0.8em] sm:text-sm font-medium py-1 truncate">{title}</h2>
+          <p className="text-[0.6em] sm:text-xs font-light truncate w-full">{description}</p>
           <span className="text-[0.6em] sm:text-xs rounded-lg bg-point px-2 py-0.5 my-3">{category}</span>
         </div>
       </section>
