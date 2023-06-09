b1378fb177a4155462709dc98e2880c611833dad
yjleeinkr <lucy.lee930216@gmail.com>
2023-06-09 12:30:39 +0900

    layout 수정: footer 고정 시 children 감싸는 main 태그에 flex-grow: 1 적용

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p className="text-center text-[3vw] sm:text-sm text-secondary py-2">
+        Be a better version of myself than yesterday | © 2023 YJ
+      </p>
+    </footer>
+  );
+}
