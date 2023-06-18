80d28c1d429752bf2645b9a7149bdc2d843170fb
yjleeinkr <lucy.lee930216@gmail.com>
2023-06-18 22:41:19 +0900

    refac: posts/[slug] 페이지 내 컴포넌트 분리 완료

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,9 +1,7 @@
 import Image from 'next/image';
-import Link from 'next/link';
-import { FcCalendar } from 'react-icons/fc';
-import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 import { getPostContents } from '@/service/posts';
-import MarkdownRenderer from '@/components/MarkdownRenderer';
+import PostContent from '@/components/PostContent';
+import AdjacentPostCard from '@/components/AdjacentPostCard';
 
 type ParamsProps = {
   params: {
@@ -15,7 +13,7 @@ export default async function PostPage({ params: { slug } }: ParamsProps) {
   const { postList, markdown } = await getPostContents(slug);
   const { prev, current, next } = postList;
   return (
-    <article className="max-w-[95%] md:max-w-[85%] xl:max-w-[80%] m-zero-auto my-5 sm:my-10 h-full bg-lightgrey rounded-lg overflow-clip">
+    <article className="max-w-[95%] md:max-w-[85%] xl:max-w-[80%] m-zero-auto my-5 sm:my-10 bg-lightgrey rounded-lg overflow-clip">
       <Image
         src={`/img/posts/${current.path}.png`}
         alt="thumbnail"
@@ -23,33 +21,10 @@ export default async function PostPage({ params: { slug } }: ParamsProps) {
         height={100}
         className="object-cover w-full h-[20vh] sm:h-[25vh] lg:h-[35vh]"
       />
-      <main className="px-5 py-1">
-        <p className="flex items-center justify-end text-sm text-secondary py-2">
-          <FcCalendar />
-          <span className="px-1">{current.date}</span>
-        </p>
-        <div className="py-3">
-          <h1 className="text-3xl font-bold">{current.title}</h1>
-          <p className="text-sm font-medium py-1">{current.description}</p>
-          <hr className="border-0 border-b-4 w-[20%] mt-2 border-highlight" />
-        </div>
-        <MarkdownRenderer markdown={markdown} />
-      </main>
+      <PostContent current={current} markdown={markdown} />
       <div className="flex w-full justify-between items-center">
-        <Link href={`/posts/${prev.path}`} className={`relative text-center w-[50%] p-10`}>
-          <div className={`bg-[url(/img/posts/${prev.path}.png)] w-full h-full`}></div>
-          <FaArrowLeft className="absolute left-10 top-[40%] text-highlight text-3xl" />
-          <h2 className="font-bold text-lg">{prev.title}</h2>
-          <p className="text-xs">{prev.description}</p>
-        </Link>
-        <Link
-          href={`/posts/${next.path}`}
-          className={`relative text-center w-[50%] p-10 bg-[url(/img/posts/${next.path}.png)]`}
-        >
-          <h2 className="font-bold text-lg">{next.title}</h2>
-          <p className="text-xs">{next.description}</p>
-          <FaArrowRight className="absolute right-10 top-[40%] text-highlight text-3xl" />
-        </Link>
+        <AdjacentPostCard post={prev} type="prev" />
+        <AdjacentPostCard post={next} type="next" />
       </div>
     </article>
   );
