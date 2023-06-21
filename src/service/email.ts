e69a9f8bd7d0365aca27695242672bbc3233d255
yjleeinkr <lucy.lee930216@gmail.com>
2023-06-22 00:11:43 +0900

    refac: nodemailer 사용하여 이메일 보내는 기능 route에서 분리하여 함수 세분화

--- src/service/email.ts
+++ src/service/email.ts
@@ -1,12 +1,27 @@
-import axios from 'axios';
+import nodemailer from 'nodemailer';
+import { FormType } from './contact';
 
-type FormType = {
-  email: string;
-  subject: string;
-  text: string;
-};
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_ID,
+    pass: process.env.EMAIL_PW,
+  },
+});
 
-export async function sendEmail(emailForm: FormType) {
-  const res = await axios.post('/api/email', emailForm);
-  return res;
+export function sendEmail({ email, subject, text }: FormType) {
+  const mailData = {
+    to: process.env.EMAIL_ID,
+    from: email,
+    subject: `[YJ_LOG] ${subject}`,
+    html: `
+      <h1>${subject}</h1>
+      <p>sender: ${email}</p>
+      <div>${text}</div>
+    `,
+  };
+
+  return transporter.sendMail(mailData);
 }
