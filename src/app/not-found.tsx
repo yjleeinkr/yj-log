b6ff9d74e59f490fc55728f58abe316cc2d0cf37
yjleeinkr <lucy.lee930216@gmail.com>
2023-06-22 23:31:53 +0900

    refac: 404 not found 페이지 추가

---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div className="flex justify-center h-full items-center">
+      <p className="text-5xl font-bold">404 NOT FOUND 🫥</p>
+    </div>
+  );
+}
