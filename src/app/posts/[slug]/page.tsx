7586c8ec8265ddb8ea489a0999ab4997da163d2d
yjleeinkr <lucy.lee930216@gmail.com>
2023-06-22 23:33:13 +0900

    refac: 포스트별 동적인 metadata 생성 및 포스트 없을 시 404페이지 렌더

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,7 +1,8 @@
 import Image from 'next/image';
-import { getPostContents } from '@/service/posts';
+import { getPostContents, getPosts } from '@/service/posts';
 import PostContent from '@/components/PostContent';
 import AdjacentPostCard from '@/components/AdjacentPostCard';
+import { notFound } from 'next/navigation';
 
 type ParamsProps = {
   params: {
@@ -9,8 +10,23 @@ type ParamsProps = {
   };
 };
 
+export async function generateMetadata({ params: { slug } }: ParamsProps) {
+  const posts = await getPosts();
+  const post = posts.find(post => post.path === slug);
+  if (!post)
+    return {
+      title: `Not Found`,
+    };
+
+  return {
+    title: post.title,
+    description: post.description,
+  };
+}
+
 export default async function PostPage({ params: { slug } }: ParamsProps) {
   const { postList, markdown } = await getPostContents(slug);
+  if (!postList || !markdown) notFound();
   const { prev, current, next } = postList;
   return (
     <article className="max-w-[95%] md:max-w-[85%] xl:max-w-[80%] m-zero-auto my-5 sm:my-10 bg-lightgrey rounded-lg overflow-clip">
