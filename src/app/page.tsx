973024638e87ad63073138a0bf042554bec7a8e6
yjleeinkr <lucy.lee930216@gmail.com>
2023-08-11 02:34:32 +0900

    wip: my log 페이지 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
-import Profile from '@/components/Profile';
+// import Profile from '@/components/Profile';
 import FeaturedPosts from '@/components/FeaturedPosts';
-import PostsSlide from '@/components/PostsSlide';
 
 export default function Home() {
   return (
